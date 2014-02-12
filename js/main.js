840738e6bcc784a5292b725f6286b338ca96acc7
Chinthaka Godawita <chinthaka.godawita@demonzmedia.com>
2014-02-12 12:09:50 +1100

    Google Analytics events and WIP of active user count.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
-(function ($, mixpanel) {
+(function ($, mixpanel, _gaq) {
+  var active_timer;
+
   $(document).ready(function () {
     // Track opt-out.
     $('a.opt-out').click(function (event) {
+      _gaq.push(['_trackEvent', 'Fishburners Trial', 'Opted out', 'Fishburners Trial']);
       mixpanel.track('Opted out of Fishburners trial');
     });
 
     // Track subscribe.
     $('#mc-embedded-subscribe').click(function (event) {
+      _gaq.push(['_trackEvent', 'Newsletter', 'Subscribed', 'Fishburners Trial Newsletter']);
       mixpanel.track('Subscribed to Fishburners trial info')
     });
 
-    // Track scrolls.
-    new MixpanelScrollTracker({
-      attribute: 'section',
-      event: 'Scrolled to Fishburners trial info on',
-      markers: [
-        { position: 500, value: "What's going on?" },
-        { position: 700, value: "How does it work?" },
-        { position: 900, value: "Tell me more" }
-      ]
+    // Running count of active users @ Fish.
+    $.doTimeout(5000, function () {
+      $.get('http://www.beeanalytics.com.au/api/web/index.php/v1/fish-active', function (data) {
+        console.log("hola datevid");
+        console.log(data);
+      });
+
+      // Poll every five seconds.
+      return true;
     });
+
   });
-})(jQuery, mixpanel);
+})(jQuery, mixpanel, _gaq);
